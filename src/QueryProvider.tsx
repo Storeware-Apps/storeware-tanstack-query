4896bb1533ae76ed280592c84aed7cebc9fed0bb
jnahian <nahian048@gmail.com>
2025-01-08 17:00:48 +0600

    Enhance QueryClient configuration with custom QueryCache and MutationCache

--- src/QueryProvider.tsx
+++ src/QueryProvider.tsx
@@ -1,7 +1,15 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import React, { ReactNode } from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache(),
+  mutationCache: new MutationCache(),
+});
 
 interface QueryProviderProps {
   children: ReactNode;
