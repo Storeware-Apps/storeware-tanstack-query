ea2a7df8a7f1c9ff6d04ae2a458fca6a807c5bcb
jnahian <nahian048@gmail.com>
2025-01-09 12:15:03 +0600

    Export TanStack Query types and update version to 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from "@tanstack/react-query";
 export { QueryProvider } from "./QueryProvider";
 export { useAppMutation } from "./useAppMutation";
 export { useAppQuery } from "./useAppQuery";
