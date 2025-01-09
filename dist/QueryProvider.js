fdbbbfb2109e8af91bb7d70089552353670ce583
jnahian <nahian048@gmail.com>
2025-01-09 10:34:27 +0600

    build

--- dist/QueryProvider.js
+++ dist/QueryProvider.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.QueryProvider = void 0;
 const react_query_1 = require("@tanstack/react-query");
 const react_1 = __importDefault(require("react"));
-const queryClient = new react_query_1.QueryClient();
+const queryClient = new react_query_1.QueryClient({
+    queryCache: new react_query_1.QueryCache(),
+    mutationCache: new react_query_1.MutationCache(),
+});
 const QueryProvider = ({ children }) => (react_1.default.createElement(react_query_1.QueryClientProvider, { client: queryClient }, children));
 exports.QueryProvider = QueryProvider;
